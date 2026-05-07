dea4df1d9348b30d1e85e0c7df27d1551c389d48
Renan Barbosa <renan_vianna7@icloud.com>
2026-05-07 20:17:21 -0300

    feat: add multiplier 5 to shadow simulator

--- src/shadow_simulator.py
+++ src/shadow_simulator.py
@@ -13,8 +13,8 @@ class ShadowSimulator:
         self.exchange = ccxt.binance({
             "enableRateLimit": True,
         })
-        self.sl_multipliers = [2, 3, 4, None]
-        self.tp_multipliers = [2, 3, 4, 6, 10, None]
+        self.sl_multipliers = [2, 3, 4, 5, 6, None]
+        self.tp_multipliers = [2, 3, 4, 5, 6, 8, 10, None]
         
     def init_db(self):
         try:
